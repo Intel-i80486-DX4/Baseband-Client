c11bcc1e101d83f6b8a28d08d30fae4b88cddeab
SmallFurGaming <hoponthespot@gmail.com>
2022-03-28 11:51:26 +0100

    No core.

--- src/main/java/me/baseband/client/Core.java
+++
@@ -1,9 +0,0 @@
-package me.baseband.client;
-
-import net.minecraftforge.fml.relauncher.FMLCorePlugin;
-
-public class Core extends FMLCorePlugin {
-    public Core(){
-        System.out.println("BaseBand Core. (Useless)");
-    }
-}
